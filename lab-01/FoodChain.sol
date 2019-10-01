88fd0ac113acf611c5fa66ffb99b1d35f60d89f9
PeterVanNostrand <petermvannostrand@gmail.com>
2019-10-01 17:08:08 -0400

    lab1: adding veto, open/close poll

--- lab-01/FoodChain.sol
+++ lab-01/FoodChain.sol
@@ -11,6 +11,9 @@ contract FoodChain {
     struct Proposal {
         bool isVetoed;
     }
+    enum Phase {init, voting, closed}
+    Phase state;
+    enum statusCode {success, notAllowed, incorrectState, invalidParameter}
 
     address organizer;
     mapping(address => Voter) voters;
@@ -18,46 +21,74 @@ contract FoodChain {
     uint numVoters;
     Proposal[] proposals;
 
-    /// Create a new poll with $(_numProposals) different proposals.
-    constructor(uint8 _numProposals) public {
-        // The person who creates the poll is the organizer
-        organizer = msg.sender;
-        proposals.length = _numProposals;
+    /// Create a new poll with numProposals different proposals
+    constructor(uint8 numProposals) public {
+        organizer = msg.sender; // The person who creates the poll is the organizer
+        proposals.length = numProposals;
         numVoters = 0;
+        state = Phase.init;
         // Initialize all proposals as not vetoed
-        for (uint8 prop = 0; prop < proposals.length; prop++){
-            proposals[prop].isVetoed = false;
-        }
+        for (uint8 prop = 0; prop < proposals.length; prop++) proposals[prop].isVetoed = false;
     }
 
-    function invite(address voterAddress, bool allowVeto) public {
-        if (msg.sender != organizer || voters[voterAddress].isInvited) return;
+    function invite(address voterAddress, bool allowVeto) public returns(statusCode code) {
+        if(msg.sender != organizer) return statusCode.notAllowed; // sender is not organizer
+        if(state!=Phase.init && state!=Phase.voting) return statusCode.incorrectState; // poll is not ready for invites or is closed
+        if(voters[voterAddress].isInvited) return statusCode.success; // voter is already invited
         voters[voterAddress].canVeto = allowVeto;
         voters[voterAddress].isInvited = true;
         voters[voterAddress].hasVoted = false;
         voters[voterAddress].willAttend = false; // assume invited person will not attend
         voterAddresses[numVoters] = voterAddress;
         numVoters += 1;
+        return statusCode.success;
     }
 
-    function rsvp(bool intent) public{
-        if (!voters[msg.sender].isInvited) return; // if voter isn't invited they cannot rsvp
+    function rsvp(bool intent) public returns(statusCode code) {
+        if(!voters[msg.sender].isInvited) return statusCode.notAllowed; // if voter isn't invited they cannot rsvp
+        if(state != Phase.voting) return statusCode.incorrectState; // rsvp only allowed during voting
         voters[msg.sender].willAttend = intent; // set the voter rsvp status, must be true to vote
+        return statusCode.success;
     }
 
-    function vote(int8[] memory votesArray) public {
+    function vote(int8[] memory votesArray) public returns(statusCode code) {
         Voter storage sender = voters[msg.sender];
         // To vote, the sender must be invited, be attending, and have the correct # of up/down votes
-        if (!sender.isInvited || !sender.willAttend || votesArray.length != proposals.length) return;
+        if (!sender.isInvited || !sender.willAttend) return statusCode.notAllowed;
+        if(state != Phase.voting) return statusCode.incorrectState;
+        if(votesArray.length != proposals.length) return statusCode.invalidParameter;
         sender.votes = votesArray; // store the number
         sender.hasVoted = true;
+        return statusCode.success;
     }
 
-    function winningProposal() public view returns (uint8 _winningProposal) {
+    function veto(uint8 proposalID) public returns(statusCode code) {
+        Voter storage sender = voters[msg.sender];
+        // must be invited, attending voter, with veto power
+        if (!sender.isInvited || !sender.willAttend || !sender.canVeto) return statusCode.notAllowed;
+        if(state != Phase.voting) return statusCode.incorrectState;
+        if(proposalID > proposals.length-1) return statusCode.invalidParameter; // no such proposal
+        proposals[proposalID].isVetoed = true;
+        return statusCode.success;
+    }
+
+    function openPoll() public returns(statusCode code) {
+        if(msg.sender != organizer) return statusCode.notAllowed;
+        state = Phase.voting;
+        return statusCode.success;
+    }
+
+    function closePoll() public returns (statusCode code) {
+        if(msg.sender != organizer) return statusCode.notAllowed;
+        state = Phase.closed;
+        return statusCode.success;
+    }
+
+    function winningProposal() public view returns (int winnerID) {
         int[] memory propVotes = new int[](proposals.length);
-        
-        // For every voter
-        for (uint i = 0; i < numVoters; i++){
+
+        // Count the votes for each prproposal
+        for (uint i = 0; i < numVoters; i++){ // For every voter
             address a = voterAddresses[i];
             Voter memory v = voters[a];
             // if voter is "valid" and has voted add their votes
@@ -69,13 +100,17 @@ contract FoodChain {
                 }
             }
         }
-        
+
+        // Determine wining proposal
         int winningVoteCount = 0;
+        int winningProp = -1;
         for (uint8 prop = 0; prop < proposals.length; prop++){
+            if(proposals[prop].isVetoed) continue; // vetoed proposals can't win
             if (propVotes[prop] > winningVoteCount) {
                 winningVoteCount = propVotes[prop];
-                _winningProposal = prop;
+                winningProp = prop;
             }
         }
+        return winningProp;
     }
 }
